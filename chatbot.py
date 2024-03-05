823b2db69c0a7e595ed3392b3fc7e7cd2a92bfda
Vichitravir Dwivedi <93407819+vichitravird@users.noreply.github.com>
2024-03-04 19:57:25 -0600

    update 1 chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,11 @@
 pip install openai
+from openai import OpenAI
+user=OpenAI(api_key='sk-')
+gptmodel="gpt-3.5-turbo"
+userrole="user"
+inputquestion=input("Type your Question here")
+response=user.chat.completions.create(
+model=gptmodel,messages=[
+    {"role": userrole, "content": inputquestion}
+])
+response.choices[0].message.content
